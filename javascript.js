c57442b1a4ccb530f935d946bd0a465260743b85
tikiid <106263002+tikiid@users.noreply.github.com>
2023-04-04 02:00:41 +0200

    changer les svg en png
    essaie de rendre les svg cliquable mais problème. utilisation de JS mais je ne comprends po ...

--- javascript.js
+++ javascript.js
@@ -23,3 +23,30 @@ window.smoothScroll = function(target) {
     // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
+
+const svgTwitter = document.getElementById("link-to-twitter")
+const svgLinkedin = document.getElementById("link-to-linkedin")
+const svgInstagram = document.getElementById("link-to-instagram")
+const svgGithub = document.getElementById("link-to-github")
+
+const linkTwitter = 0
+const linkLinkedin = "https://www.linkedin.com/in/thibaut-schweitzer-635223201/"
+const linkInstagram = 'https://twitter.com/Tikiid_'
+const linkGithub = 'https://github.com/tikiid'
+
+function goToUrl(url){
+    window.open(url, '_blank').focus()
+}
+
+// svgLinkedin.addEventListener('click', function(){
+//     window.open(linkLinkedin, '_blank').focus()
+// })
+
+function showMsg() {
+    console.log("Bonjour");
+}
+
+svgLinkedin.addEventListener('click', showMsg)
+
+
+
